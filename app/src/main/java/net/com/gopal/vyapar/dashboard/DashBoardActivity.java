55525adf32c8abd02ad210948176bd0032fd4e86
gopa11313 <gopalaaa9819525993@gmail.com>
2022-01-01 11:07:54 +0545

    done with dashboard design

--- app/src/main/java/net/com/gopal/vyapar/dashboard/DashBoardActivity.java
+++ app/src/main/java/net/com/gopal/vyapar/dashboard/DashBoardActivity.java
@@ -3,11 +3,13 @@ package net.com.gopal.vyapar.dashboard;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.FragmentManager;
 
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import net.com.gopal.vyapar.DuoMenuAdapter;
@@ -24,12 +26,11 @@ public class DashBoardActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TextView toolbar_text;
     private DuoDrawerLayout duodrawerLayout;
-    private DuoLock duoLock;
-    private ViewHolder mViewHolder;
-    private DuoMenuAdapter mMenuAdapter;
-    private DashBoardActivity.SetName setName;
     private TextView userName;
+    private FrameLayout container;
     private ArrayList<String> mTitles = new ArrayList<>();
+    private FragmentManager fm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +39,7 @@ public class DashBoardActivity extends AppCompatActivity {
             setTheme(R.style.AppTheme);
             toolbar = findViewById(R.id.toolbar_main);
             userName = findViewById(R.id.userNameDrawer);
+            container = findViewById(R.id.container);
             toolbar.post(new Runnable() {
                 @Override
                 public void run() {
@@ -45,96 +47,14 @@ public class DashBoardActivity extends AppCompatActivity {
                     toolbar.setNavigationIcon(d);
                 }
             });
-//            mTitles = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.ld_activityScreenTitles)));
-//            mViewHolder = new ViewHolder();
-//            duoNavigationLock();
-//            // handleToolbar();
-//            handleMenu();
-//            handleDrawer();
-//            setName = name -> {
-//                userName.setText(name);
-//
-//            };
+            DashboardFragment fragment = new DashboardFragment();
+            fm = getSupportFragmentManager();
+            fm.beginTransaction()
+                    .replace(R.id.container, fragment).commit();
 
-        }catch (Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    private void handleDrawer() {
-        final DuoDrawerToggle duoDrawerToggle = new DuoDrawerToggle(DashBoardActivity.this,
-                mViewHolder.mDuoDrawerLayout,
-                mViewHolder.mToolbar,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close
-        );
-
-        mViewHolder.mDuoDrawerLayout.setDrawerListener(duoDrawerToggle);
-        duoDrawerToggle.syncState();
-
-      /*  ValueAnimator anim = ValueAnimator.ofFloat(0,1);
-        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                float slideOffset = (Float) valueAnimator.getAnimatedValue();
-                duoDrawerToggle.onDrawerSlide(mViewHolder.mDuoDrawerLayout, slideOffset);
-            }
-        });
-
-
-        anim.setInterpolator(new DecelerateInterpolator());
-        // You can change this duration to more closely match that of the default animation.
-        anim.setDuration(100);
-        anim.start();
-*/
-    }
-    public interface SetName {
-        void setName(String name);
-    }
-//    @Override
-//    public void onFooterClicked() {
-//
-//    }
-//
-//    @Override
-//    public void onHeaderClicked() {
-//
-//    }
-//
-//    @Override
-//    public void onOptionClicked(int position, Object objectClicked) {
-//
-//    }
-    private void duoNavigationLock() {
-        duoLock = new DuoLock() {
-            @Override
-            public void lockNavigation(int lockmode) {
-                duodrawerLayout.setDrawerLockMode(DuoDrawerLayout.LOCK_MODE_LOCKED_CLOSED);
-                mViewHolder.mDuoMenuView.setVisibility(View.GONE);
-                duodrawerLayout.closeDrawer();
-            }
-        };
-    }
-    public interface DuoLock extends Serializable {
-        void lockNavigation(int lockmode);
-    }
-    private class ViewHolder {
-        private DuoDrawerLayout mDuoDrawerLayout;
-        private DuoMenuView mDuoMenuView;
-        private Toolbar mToolbar;
-
-        ViewHolder() {
-            mDuoDrawerLayout = findViewById(R.id.drawer);
-            mDuoMenuView = (DuoMenuView) mDuoDrawerLayout.getMenuView();
-            mToolbar = findViewById(R.id.toolbar_main);
-            mDuoMenuView.getHeaderView().setBackgroundColor(Color.WHITE);
-        }
-    }
-    private void handleMenu() {
-
-        mMenuAdapter = new DuoMenuAdapter(mTitles);
-        mViewHolder.mDuoMenuView.setOnMenuClickListener((DuoMenuView.OnMenuClickListener) this);
-        mViewHolder.mDuoMenuView.setAdapter(mMenuAdapter);
-
-    }
-
 }
---
+++ app/src/main/java/net/com/gopal/vyapar/dashboard/DashboardFragment.java
@@ -0,0 +1,40 @@
+package net.com.gopal.vyapar.dashboard;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.com.gopal.vyapar.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DashboardFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState)  {
+            return inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
